0ce696a8d29518fad7d813f7ade5ee8097aa55c1
wesleyfsousa01 <wesleyfsouza10@gmail.com>
2023-02-12 17:07:02 -0300

    Mentoria e curso

---
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,26 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Descrição Curso Js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria  = new Mentoria();
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("Descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
